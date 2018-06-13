9c85add99cf705b5c771985fcec8c571c71cee4e
Ahmad <elemamm51@gmail.com>
2018-06-14 01:02:07 +0200

    okok

--- app/src/main/java/domain/com/recipes/Offline.java
+++ app/src/main/java/domain/com/recipes/Offline.java
@@ -5,7 +5,7 @@ import android.os.Bundle;
 import android.widget.ImageView;
 
 public class Offline extends AppCompatActivity {
-ImageView menu1,menu2;
+ImageView menu1,menu2,menu3,menu4,menu5,menu6,menu7,menu8,menu9,menu10,menu11,menu12,menu13;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,6 +14,17 @@ ImageView menu1,menu2;
         getSupportActionBar().hide();
         menu1 = findViewById(R.id.menu1);
         menu2 = findViewById(R.id.menu2);
+        menu3 = findViewById(R.id.menu3);
+        menu4 = findViewById(R.id.menu4);
+        menu5 = findViewById(R.id.menu5);
+        menu6 = findViewById(R.id.menu6);
+        menu7 = findViewById(R.id.menu7);
+        menu8 = findViewById(R.id.menu8);
+        menu9 = findViewById(R.id.menu9);
+        menu10 = findViewById(R.id.menu10);
+        menu11= findViewById(R.id.menu11);
+        menu12 = findViewById(R.id.menu12);
+        menu13= findViewById(R.id.menu13);
 
     }
 }
